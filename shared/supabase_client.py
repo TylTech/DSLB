06589c509b880815c67bb7e76e40384584fe04f3
OrbachDSL <orbachdsl@gmail.com>
2025-04-07 12:36:54 -0400

    Moved all stuff to new repo

--- shared/supabase_client.py
+++ shared/supabase_client.py
@@ -2,7 +2,7 @@ from supabase import create_client
 import os
 
 # Load from environment or hardcode for now
-SUPABASE_URL = os.getenv("SUPABASE_URL", "https://bwbpqfqlmygtdgknxtco.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3YnBxZnFsbXlndGRna254dGNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4ODIyMTEsImV4cCI6MjA1OTQ1ODIxMX0.VlSTC-zr0COPginV8CVjpc1auFldlfAws2bos5-VjEg")
+SUPABASE_URL = os.getenv("SUPABASE_URL", "https://ilzmnjncspqnlfblludv.supabase.co")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsem1uam5jc3BxbmxmYmxsdWR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNDIwOTMsImV4cCI6MjA1OTYxODA5M30.MprmH1nNY9bY28dhN9mo4lHhW0dwA6HEHd9nUbIa2js")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
